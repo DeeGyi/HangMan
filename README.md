20c8beb7464ee6f2d9f0031556b395b9bb010438
deegyi <dee_gyi@live.ca>
2022-08-12 10:05:20 -0400

    updated

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # HangMan
+
+Hangman Horror Story
+Horror themed Hangman game that quizzes your trivia on popular horror movies of all time.
+
+
+
+☐ Screenshot(s): Images of your actual game.
+
+Technologies Used: List of the technologies used, e.g., JavaScript, HTML, CSS...
+
+☐ Getting Started: In this section include the link to your deployed game and any instructions you deem important.
+
+☐ Next Steps: Planned future enhancements (icebox items).
+
+Note: Don't underestimate the value of a well crafted README.md. The README.md introduces your project to prospective employers and forms their first impression of your work!
---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+// ICEBOX
+// bodyParts for each wrong answer you get
+// wrongGuessLetters
+// typing letters out
+// alphabets
+
+// /*----- constants -----*/
+
+const alphabet = document.getElementById('alphabet');
+const container = document.getElementById('container');
+const hangman = document.querySelectorAll('h-container');
+const words = document.querySelector('guess');
+const resetGame = document.getElementById('resetBtn');
+const enter = document.getElementById('enter')
+
+const wordGuess = document.querySelector(".wordGuess");
+
+// /*----- app's state (variables) -----*/
+var wrongGuess = "";
+var movies = ["Exocist", "Conjuring", "Shining", "Grudge", "Taxes", "Psycho" ];
+
+// /*----- cached element references -----*/
+
+document.getElementById('enter').onclick = function () {
+    var letterBox = document.getElementById('letter-key').value;
+   
+}
+letterBox()
+
+document.getElementById('stand').src="./Source/hangman0.jpg.jpg"
+document.getElementById('head').style.display = 'none';
+document.getElementById('body').style.display = 'none';
+document.getElementById('rArm').style.display = 'none';
+document.getElementById('lArm').style.display = 'none';
+document.getElementById('rLeg').style.display = 'none';
+document.getElementById('lLeg').style.display = 'none';
+// /*----- event listeners -----*/
+
+resetGame.addEventListener("click", ()  => location.reload());
+
+// /*----- functions -----*/
+
+
+
+
+function word() {
+    let ratedRMovie = movies[Math.floor(Math.random() * movies.length)];
+    let horror = ratedRMovie.horror;
+    console.log(horror);
+
+    let movie = "";
+    for (let i = 0; i < horror.length; i++) {
+        movie = ``;
+    }
+    wordGuess.innerHTML = movie;
+}
+    word();
+
+   
